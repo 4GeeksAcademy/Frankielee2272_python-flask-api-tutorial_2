77807a5d45b786f838cfa7f823ed15860ad9d32d
Frank Trujillo <155175699+Frankielee2272@users.noreply.github.com>
2024-04-18 21:38:21 +0000

    At lesson 4

---
+++ src/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3245, debug=True)
+
+
+@app.route('/myroute', methods=['GET'])
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/todos', methods=['GET'])
+def todos():
+    return '<h1>Hello!</h1>'
